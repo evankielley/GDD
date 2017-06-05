f3f94a57a7d65eedc6722c4fbc090879e6f84f31
OAbr <oabramov@mun.ca>
2017-06-05 14:45:51 -0230

    Added some comments to gdd.py

--- gdd.py
+++ gdd.py
@@ -2,12 +2,14 @@ import argparse
 import glob
 import sys
 import pandas as pd
+# If no folder is specified use current folder
 if(len(sys.argv) == 3):
     parser = argparse.ArgumentParser()
     parser.add_argument("tbase", type=int)
     parser.add_argument("tupper", type=int)
     args = parser.parse_args()
     inputFolder = './'
+# If folder for input data is specified
 elif(len(sys.argv) == 4):
     parser = argparse.ArgumentParser()
     parser.add_argument("tbase", type=int)
@@ -15,25 +17,31 @@ elif(len(sys.argv) == 4):
     parser.add_argument("inputFolder", type=str)
     args = parser.parse_args()
     inputFolder = args.inputFolder
+# Need some reaction for different number of arguments
+else:
+    pass
+
 filesList = glob.glob(inputFolder+"*_temp.csv")
 for inputFileName in filesList:
     data=pd.read_csv(inputFileName)
-    #date = data['Date/Time'].copy()
-    min_temp = data['Min Temp (°C)'].copy()
-    max_temp = data['Max Temp (°C)'].copy()
-    #if(not (min_temp.size == max_temp.size)):
-    #    print("Data mismatch")
+    min_temp = data['Min Temp (°C)']
+    max_temp = data['Max Temp (°C)']
+    # may add check for different min_temp and max_temp size
+
+    # creating dataframe with the same size as min_temp
     gdd=min_temp.copy()
     for i in min_temp.keys():
-        min_temp[i] = min(args.tupper, max(args.tbase, min_temp[i]))
-        max_temp[i] = min(args.tupper, max(args.tbase, max_temp[i]))
-        gdd[i]      = (min_temp[i]+max_temp[i])/2 - args.tbase
+        # Making min_temp and max_temp to be in a range (tbase, tupper)
+        min_temp_val = min(args.tupper, max(args.tbase, min_temp[i]))
+        max_temp_val = min(args.tupper, max(args.tbase, max_temp[i]))
+        gdd[i]      = (min_temp_val+max_temp_val)/2 - args.tbase
+        # GDD is cumulative
         if(i>0):
             gdd[i] += gdd[i-1]
 
     frames_list = [data['Date/Time'], data['Min Temp (°C)'], data['Max Temp (°C)'], gdd]
-
     data = pd.concat(frames_list, axis=1, join_axes=[gdd.index])
     data.columns = ['Date', 'MinTemp', 'MaxTemp', 'GDD']
+    # forming new file name using the same template
     outputFileName = inputFileName[:-8]+"gdd.csv"
     data.to_csv(outputFileName)
