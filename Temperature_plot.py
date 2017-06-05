321de1909f89758326875ec352bf7e25169c63c0
Razyeh Behbahani <rbehbahani@mun.ca>
2017-06-05 13:43:22 -0230

    making temp plots

---
+++ Temperature_plot.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+%matplotlib inline
+%cd ./GDD
+figure=plt.figure(num=1,figsize=(16,4))
+days=np.arange(365)
+
+plt.subplot(121)
+data1=pd.read_csv('2015_Ottawa_gdd.csv')
+data2=pd.read_csv('2015_Montreal_gdd.csv')
+data3=pd.read_csv('2015_Victoria_gdd.csv')
+
+max_temp1 = data1['MaxTemp']
+max_temp2 = data2['MaxTemp']
+max_temp3 = data3['MaxTemp']
+
+plt.plot(days,max_temp1,'r.-',label='Ottawamax')
+plt.plot(days,max_temp2,'b.-',label='Montrealmax')
+plt.plot(days,max_temp3,'k.-',label='Victoriamax')
+plt.title('Three Cities Max Temp')
+plt.legend()
+
+plt.subplot(122)
+min_temp1= data1['MinTemp']
+min_temp2 = data2['MinTemp']
+min_temp3 = data3['MinTemp']
+
+plt.plot(days,min_temp1,'r-',label='Otawamin')
+plt.plot(days,min_temp2,'b-',label='Montrealmin')
+plt.plot(days,min_temp3,'k-',label='Victoriamin')
+plt.title('Three Cities Min Temp')
+plt.legend()
+plt.savefig('plot/CompareMaxMinTemp.png')
+
+plt.subplot(131)
+data1=pd.read_csv('2015_Ottawa_gdd.csv')
+data2=pd.read_csv('2015_Montreal_gdd.csv')
+data3=pd.read_csv('2015_Victoria_gdd.csv')
+max_temp = data1['MaxTemp']
+min_temp = data1['MinTemp']
+plt.plot(days,max_temp,'r.-',label='max')
+plt.plot(days,min_temp,'y-',label='min')
+plt.legend()
+plt.title('OttawaTemp')
+
+plt.subplot(132)
+max_temp = data2['MaxTemp']
+min_temp = data2['MinTemp']
+plt.plot(days,max_temp,'b.-',label='max')
+plt.plot(days,min_temp,'c-',label='min')
+plt.legend()
+plt.title('MontrealTemp')
+
+plt.subplot(133)
+max_temp = data3['MaxTemp']
+min_temp = data3['MinTemp']
+plt.plot(days,max_temp,'k.-',label='max')
+plt.plot(days,min_temp,'g-',label='min')
+plt.legend()
+plt.title('VictoriaTemp')
+plt.savefig('plot/ThreeCitiesAnnualTemp.png')
