d322b6458d05340cf23c435dee70318b4fed3845
OAbr <oabramov@mun.ca>
2017-06-05 14:01:02 -0230

    gdd.py can take <folder> as an optional argument to search input data there

--- gdd.py
+++ gdd.py
@@ -1,28 +1,39 @@
 import argparse
+import glob
+import sys
 import pandas as pd
-parser = argparse.ArgumentParser()
-parser.add_argument("tbase", type=int)
-parser.add_argument("tupper", type=int)
-parser.add_argument("inputFileName", type=str)
-args = parser.parse_args()
+if(len(sys.argv) == 3):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tbase", type=int)
+    parser.add_argument("tupper", type=int)
+    args = parser.parse_args()
+    inputFolder = './'
+elif(len(sys.argv) == 4):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tbase", type=int)
+    parser.add_argument("tupper", type=int)
+    parser.add_argument("inputFolder", type=str)
+    args = parser.parse_args()
+    inputFolder = args.inputFolder
+filesList = glob.glob(inputFolder+"*_temp.csv")
+for inputFileName in filesList:
+    data=pd.read_csv(inputFileName)
+    #date = data['Date/Time'].copy()
+    min_temp = data['Min Temp (°C)'].copy()
+    max_temp = data['Max Temp (°C)'].copy()
+    #if(not (min_temp.size == max_temp.size)):
+    #    print("Data mismatch")
+    gdd=min_temp.copy()
+    for i in min_temp.keys():
+        min_temp[i] = min(args.tupper, max(args.tbase, min_temp[i]))
+        max_temp[i] = min(args.tupper, max(args.tbase, max_temp[i]))
+        gdd[i]      = (min_temp[i]+max_temp[i])/2 - args.tbase
+        if(i>0):
+            gdd[i] += gdd[i-1]
 
-data=pd.read_csv(args.inputFileName)
-#date = data['Date/Time'].copy()
-min_temp = data['Min Temp (°C)'].copy()
-max_temp = data['Max Temp (°C)'].copy()
-#if(not (min_temp.size == max_temp.size)):
-#    print("Data mismatch")
-gdd=min_temp.copy()
-for i in min_temp.keys():
-    min_temp[i] = min(args.tupper, max(args.tbase, min_temp[i]))
-    max_temp[i] = min(args.tupper, max(args.tbase, max_temp[i]))
-    gdd[i]      = (min_temp[i]+max_temp[i])/2 - args.tbase
-    if(i>0):
-        gdd[i] += gdd[i-1]
+    frames_list = [data['Date/Time'], data['Min Temp (°C)'], data['Max Temp (°C)'], gdd]
 
-frames_list = [data['Date/Time'], data['Min Temp (°C)'], data['Max Temp (°C)'], gdd]
-
-data = pd.concat(frames_list, axis=1, join_axes=[gdd.index])
-data.columns = ['Date', 'MinTemp', 'MaxTemp', 'GDD']
-outputFileName = args.inputFileName[:-8]+"gdd.csv"
-data.to_csv(outputFileName)
+    data = pd.concat(frames_list, axis=1, join_axes=[gdd.index])
+    data.columns = ['Date', 'MinTemp', 'MaxTemp', 'GDD']
+    outputFileName = inputFileName[:-8]+"gdd.csv"
+    data.to_csv(outputFileName)
