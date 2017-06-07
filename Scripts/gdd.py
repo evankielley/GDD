3d04f3f41747e838ba4c230c32f6877069424efc
Oleksandr Abramov <oabramov@mun.ca>
2017-06-07 12:33:03 -0230

    Redesigned gdd.py to make gdd calculation as an external function

--- Scripts/gdd.py
+++ Scripts/gdd.py
@@ -2,6 +2,8 @@ import argparse
 import glob
 import sys
 import pandas as pd
+from calc_gdd import *
+
 # If no folder is specified use current folder
 if(len(sys.argv) == 3):
     parser = argparse.ArgumentParser()
@@ -20,6 +22,8 @@ elif(len(sys.argv) == 4):
 # Need some reaction for different number of arguments
 else:
     pass
+    # print("Wrong number of arguments")
+    # sys.exit()
 
 filesList = glob.glob(inputFolder+"*_temp.csv")
 for inputFileName in filesList:
@@ -28,16 +32,9 @@ for inputFileName in filesList:
     max_temp = data['Max Temp (°C)']
     # may add check for different min_temp and max_temp size
 
-    # creating dataframe with the same size as min_temp
-    gdd=min_temp.copy()
-    for i in min_temp.keys():
-        # Making min_temp and max_temp to be in a range (tbase, tupper)
-        min_temp_val = min(args.tupper, max(args.tbase, min_temp[i]))
-        max_temp_val = min(args.tupper, max(args.tbase, max_temp[i]))
-        gdd[i]      = (min_temp_val+max_temp_val)/2 - args.tbase
-        # GDD is cumulative
-        if(i>0):
-            gdd[i] += gdd[i-1]
+    gdd = calc_gdd(list(min_temp), list(max_temp), args.tbase, args.tupper)
+    # converting list to pandas DataFrame to merge later
+    gdd = pd.DataFrame.from_items([("GDD", gdd)])
 
     frames_list = [data['Date/Time'], data['Min Temp (°C)'], data['Max Temp (°C)'], gdd]
     data = pd.concat(frames_list, axis=1, join_axes=[gdd.index])
