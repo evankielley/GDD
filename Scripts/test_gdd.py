8d58bdf22eca73f4806a38ffc413bc18f8e66118
Oleksandr Abramov <oabramov@mun.ca>
2017-06-10 11:34:28 -0230

    Modify test_gdd.py for new calc_gdd.py output

--- Scripts/test_gdd.py
+++ Scripts/test_gdd.py
@@ -3,7 +3,7 @@ from numpy.testing import assert_allclose
 
 def test_all_empty():
     observed = calc_gdd([],[],10,30)
-    expected = []
+    expected = ([], [])
     assert observed == expected
 
 def test_first_empty():
@@ -33,37 +33,32 @@ def test_tbase_greater_tupper():
 
 def test_plain_data():
     observed = calc_gdd([11, 12, 13],[24, 25, 26],10,30)
-    expected = [7.5, 16. , 25.5]
+    expected = ([7.5, 8.5, 9.5], [7.5, 16. , 25.5])
     assert_allclose(observed, expected, rtol=1e-6)
 
 def test_min_less_than_tbase():
     observed = calc_gdd([1, 2, 3],[24, 25, 26],10,30)
-    expected = [7.0, 14.5, 22.5]
+    expected = ([7, 7.5, 8], [7, 14.5, 22.5])
     assert_allclose(observed, expected, rtol=1e-6)
 
 def test_max_greater_than_tupper():
     observed = calc_gdd([11, 12, 13],[34, 35, 36],10,30)
-    expected = [10.5, 21.5, 33.0]
-    assert_allclose(observed, expected, rtol=1e-6)
-
-def test_max_greater_than_tupper():
-    observed = calc_gdd([11, 12, 13],[34, 35, 36],10,30)
-    expected = [10.5, 21.5, 33.0]
+    expected = ([10.5, 11, 11.5], [10.5, 21.5, 33.0])
     assert_allclose(observed, expected, rtol=1e-6)
 
 def test_both_less_than_tbase():
     observed = calc_gdd([1, 2, 3],[4, 5, 6],10,30)
-    expected = [0. , 0. , 0.]
+    expected = ([0. , 0. , 0. ], [0. , 0. , 0.])
     assert_allclose(observed, expected, rtol=1e-6)
 
 def test_both_greater_than_tupper():
     observed = calc_gdd([31, 32, 33],[34, 35, 36],10,30)
-    expected = [20.0, 40.0, 60.0]
+    expected = ([20. , 20. , 20. ], [20.0, 40.0, 60.0])
     assert_allclose(observed, expected, rtol=1e-6)
 
 def test_min_less_than_tbase_max_greater_than_tupper():
     observed = calc_gdd([1, 2, 3],[34, 35, 36],10,30)
-    expected = [10.0, 20.0, 30.0]
+    expected = ([10. , 10. , 10. ], [10.0, 20.0, 30.0])
     assert_allclose(observed, expected, rtol=1e-6)
 
 def test_tmin_greater_tmax():
