56075ffb79e47f3d7770548448ea4d1cc49133d9
RudigerBrecht <rbrecht@mun.ca>
2017-06-13 00:28:45 -0230

    added nl effective gdd to create plots
    also changed min max plot
    -> put title to the right
    -> added y label

--- Scripts/create_plots.py
+++ Scripts/create_plots.py
@@ -1,14 +1,17 @@
+# -*- coding: utf-8 -*-
 import sys,os
 import pandas as pd
+#import matplotlib
 import matplotlib.pylab as plt
 import numpy as np
 from calc_gdd import calc_gdd
-
+from matplotlib.mlab import griddata
+from mpl_toolkits.basemap import Basemap
 from bokeh.plotting import figure, output_file, save
 from bokeh.models import HoverTool, BoxSelectTool
 
 def main():
-    global path, names, days, months
+    global path,names,days, months
     names=[]
     path = os.path.abspath("./Output")
     for file in os.listdir(path):
@@ -18,13 +21,17 @@ def main():
     days = [0,30,58,89,119,150,180,211,242,272,303,333]
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-    max_min_plot()
-    gdd_plot()
+    max_min_plot(names)
+    gdd_plot(names)
     analyze_tbase()
-    bokeh_plot_temp()
-    bokeh_plot_gdd()
 
-def max_min_plot():
+    fname = '~/GDD/Output/2015_Montreal_gdd.csv'
+    bokeh_plot_temp(path + "/" + names[0])
+    bokeh_plot_gdd(path + "/" + names[0])
+
+    map_plot_nl_gdd()
+
+def max_min_plot(names):
     plt.figure(1)
     plt.subplot(111)
     labels=[]
@@ -34,19 +41,22 @@ def max_min_plot():
         plotData=pd.read_csv(path +'/'+ fileName)
         ax=plt.figure(1,figsize=(5,n*15))
         plt.subplot(n,1,i+1)
+        plt.subplots_adjust(hspace=.5)
         plt.plot(plotData['MaxTemp'],'r')
         plt.plot(plotData['MinTemp'],'b')
-        plt.ylabel(fileName.split('_')[1])
+        plt.text(405,plotData['MaxTemp'].mean(),fileName.split('_')[1],rotation=-90)
+#        plt.ylabel(fileName.split('_')[1])
         plt.xticks(days,months)
         plt.legend(loc='upper right',prop={'size':6})
+    plt.ylabel('T in '+u'\xb0'+'C')
     plt.xticks(days,months)
     plt.xlabel('Days')
     plt.suptitle('Max and Min Temperature')
     plt.savefig('./Output/CompareMaxMinTemp.png')
 
-def gdd_plot():
+def gdd_plot(names):
     labels = []
-    plt.figure(4)
+    plt.figure(4)   
     for fileName in names:
         i = names.index(fileName) 
         plotData=pd.read_csv(path +'/'+ fileName)
@@ -83,40 +93,116 @@ def analyze_tbase():
     plt.legend(data.columns,loc='upper left')
     plt.savefig('./Output/AnalyzeTbase.png')
 
-def bokeh_plot_temp():
+def bokeh_plot_temp(fname):
 
-    df = pd.read_csv(path + "/" + names[0])
-    xdata = df.index.values
+    df = pd.read_csv(fname)
 
     hover = HoverTool(tooltips=[("index", "$index"),("Temp", "$y"),])
     p = figure(title = "Montreal Temperature 2015", x_axis_type="datetime", tools=[hover, "pan,reset,resize,wheel_zoom"])
     p.xaxis.axis_label = 'Date'
     p.yaxis.axis_label = 'Temperature (°C)'
+
+    #xdata = np.array(df['Date'], dtype=np.datetime64)
+    xdata = df.index.values
+
     p.line(xdata, df["MaxTemp"], legend="Max Temp", line_color = "red")
     p.circle(xdata, df["MaxTemp"], legend="Max Temp", fill_color="red", line_color="red", size=6)
+
     p.line(xdata, df["MinTemp"], legend="Min Temp")
     p.circle(xdata, df["MinTemp"], legend="Min Temp", fill_color="white", size=8)
 
     new_fname =  os.path.dirname(os.path.realpath(__file__)) + "/../Output/" + "bokeh_temp.html"
     output_file(new_fname, title="Min_Max plot")
+
     save(p)
 
 
-def bokeh_plot_gdd():    
+def bokeh_plot_gdd(fname):    
 
-    df = pd.read_csv(path + "/" + names[0])
-    xdata = df.index.values
+    df = pd.read_csv(fname)
     
     hover = HoverTool(tooltips=[("Index", "$index"),("GDD", "$y"),])
     p = figure(title = "Montreal GDD 2015", tools=[hover, "pan,reset,resize,wheel_zoom"])
     p.xaxis.axis_label = 'Date'
     p.yaxis.axis_label = 'GDD'
+
+    xdata = np.arange(0, len(df["GDD"]))
+
     p.line(xdata, df["GDD"], legend="GDD", line_color = "red")
     p.circle(xdata, df["GDD"], legend="GDD", fill_color="red", line_color="red", size=6)
 
+    
     new_fname =  os.path.dirname(os.path.realpath(__file__)) + "/../Output/" + "bokeh_gdd.html"
     output_file(new_fname, title="GDD plot")
+
     save(p)
+
+def map_plot_nl_gdd():
+    # read data from csv file
+    dataMin=pd.read_csv('./Input/tempMin.csv',skiprows=7)
+    dataMax=pd.read_csv('./Input/tempMax.csv',skiprows=7)
+
+    dataMean=pd.read_csv('./Input/tempMean.csv',skiprows=7)
+
+    year = 1972 #1971-2000
+    month=[' january', ' february', ' march', ' april', ' may', ' june', ' july', ' august', ' september', ' october', ' november', ' december']
+
+    lat=dataMin[dataMin[' year']==year]['lat']       
+    lon=dataMin[dataMin[' year']==year][' lon']
+    tmin=dataMin[dataMin[' year']==year][month]
+    tmax=dataMax[dataMax[' year']==year][month]
+    tmean=dataMean[dataMean[' year']==year][month]
+
+    gdd=[]
+    for index, row in tmean.iterrows():    
+        gdd.append(calc_gdd(tmin.loc[index],tmax.loc[index],10,30)[1][-1])
+    
+
+    lat=list(lat)
+    lon=list(lon)
+
+    # plot map
+    plt.figure(figsize=(20,10))
+
+    latMin=min(lat)
+    latMax=max(lat)
+    lonMin=min(lon)
+    lonMax=max(lon)
+
+    map = Basemap(projection='merc', lat_0 = latMin, lon_0 = lonMax,
+        resolution = 'h', area_thresh = 0.1,
+        llcrnrlon=lonMin, llcrnrlat=latMin,
+        urcrnrlon=lonMax, urcrnrlat=latMax)
+ 
+    map.drawcoastlines()
+    map.drawcountries()
+    #map.fillcontinents(color = 'coral')
+    map.drawmapboundary()
+      
+    # Define a colormap
+    jet = plt.cm.get_cmap('jet')
+    # Transform points into Map's projection
+    x,y = map(lon, lat)
+    # Color the transformed points!
+    sc = plt.scatter(x,y, c=gdd, vmin=min(gdd), vmax =max(gdd), cmap=jet) # ,s=700, edgecolors='none'
+    # And let's include that colorbar
+
+    # interpolate data points
+    numIndexes = 500
+    xi = np.linspace(np.min(x), np.max(x),numIndexes)
+    yi = np.linspace(np.min(y), np.max(y),numIndexes)
+
+    DEM = griddata(x, y, gdd, xi, yi,interp='linear')
+
+    map.imshow(DEM,cmap =jet,origin='lower') #cmap ='RdYlGn_r'
+    map.drawlsmask(land_color=(0, 0, 0, 0), ocean_color='white', lakes=True)
+    cbar = plt.colorbar(sc, shrink = .5)
+    #cbar.set_label(temp)
+
+    plt.title('Acumulated GDD of the year '+str(year))
+
+    plt.savefig('./Output/gddMapPlotNL.png')
+
 
 if __name__ == '__main__':
     main()
