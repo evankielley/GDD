012546617bceaedc9516f6fb05505db440e67247
evankielley <evankielley@gmail.com>
2017-06-12 22:06:09 -0230

    added a main function and the bokeh html functions

--- Scripts/create_plots.py
+++ Scripts/create_plots.py
@@ -1,25 +1,37 @@
 import sys,os
 import pandas as pd
-import matplotlib
-from matplotlib import pylab as plt
+#import matplotlib
+import matplotlib.pylab as plt
+import numpy as np
 from calc_gdd import calc_gdd
 
-names=[]
-path = os.path.abspath("./Output")
-for file in os.listdir(path):
-    if file.endswith("gdd.csv"):
-        names.append(file)
-n=len(names)
-days = [0,30,58,89,119,150,180,211,242,272,303,333]
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-plt.figure(1)
-plt.subplot(111)
-plt.figure(2)
-labels=[]
+from bokeh.plotting import figure, output_file, save
+from bokeh.models import HoverTool, BoxSelectTool
+
+def main():
+    global path,names,days, months
+    names=[]
+    path = os.path.abspath("./Output")
+    for file in os.listdir(path):
+        if file.endswith("gdd.csv"):
+            names.append(file)
+
+    days = [0,30,58,89,119,150,180,211,242,272,303,333]
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+    max_min_plot(names)
+    gdd_plot(names)
+    analyze_tbase()
+
+    fname = '~/GDD/Output/2015_Montreal_gdd.csv'
+    bokeh_plot_temp(path + "/" + names[0])
+    bokeh_plot_gdd(path + "/" + names[0])
 
 def max_min_plot(names):
     plt.figure(1)
     plt.subplot(111)
+    labels=[]
+    n=len(names)
     for fileName in names:
         i = names.index(fileName) 
         plotData=pd.read_csv(path +'/'+ fileName)
@@ -35,10 +47,8 @@ def max_min_plot(names):
     plt.suptitle('Max and Min Temperature')
     plt.savefig('./Output/CompareMaxMinTemp.png')
 
-max_min_plot(names)
-
 def gdd_plot(names):
-
+    labels = []
     plt.figure(4)
     for fileName in names:
         i = names.index(fileName) 
@@ -53,10 +63,7 @@ def gdd_plot(names):
     plt.xticks(days,months)
     plt.savefig('./Output/CumulativeGDD.png')
 
-gdd_plot(names)
-
 def analyze_tbase():
-
     tbase = 10; tupper = 30
     tmin = 9; tmax = 12
     df = pd.read_csv(path +'/'+ names[0])
@@ -65,7 +72,6 @@ def analyze_tbase():
     gdd = calc_gdd(list(min_temp), list(max_temp), tmin-1, tupper)
     col_name = "Tbase: {}".format(tmin-1)
     data = pd.DataFrame({col_name: gdd[1]})
-
     for tbase in range(tmin,tmax):
         gdd = calc_gdd(list(min_temp), list(max_temp), tbase, tupper)
         col_name = "Tbase: {}".format(tbase)
@@ -74,7 +80,56 @@ def analyze_tbase():
     plt.figure(5)
     plt.xticks(days,months)
     plt.plot(data)
+    plt.title("Effect of Tbase on Growing Degree Days (GDD)")
+    plt.xlabel("Year")
+    plt.ylabel("Cumulative GDD")
     plt.legend(data.columns,loc='upper left')
     plt.savefig('./Output/AnalyzeTbase.png')
 
-analyze_tbase()
+def bokeh_plot_temp(fname):
+
+    df = pd.read_csv(fname)
+
+    hover = HoverTool(tooltips=[("index", "$index"),("Temp", "$y"),])
+    p = figure(title = "Montreal Temperature 2015", x_axis_type="datetime", tools=[hover, "pan,reset,resize,wheel_zoom"])
+    p.xaxis.axis_label = 'Date'
+    p.yaxis.axis_label = 'Temperature (°C)'
+
+    #xdata = np.array(df['Date'], dtype=np.datetime64)
+    xdata = df.index.values
+
+    p.line(xdata, df["MaxTemp"], legend="Max Temp", line_color = "red")
+    p.circle(xdata, df["MaxTemp"], legend="Max Temp", fill_color="red", line_color="red", size=6)
+
+    p.line(xdata, df["MinTemp"], legend="Min Temp")
+    p.circle(xdata, df["MinTemp"], legend="Min Temp", fill_color="white", size=8)
+
+    new_fname =  os.path.dirname(os.path.realpath(__file__)) + "/../Output/" + "bokeh_temp.html"
+    output_file(new_fname, title="Min_Max plot")
+
+    save(p)
+
+
+def bokeh_plot_gdd(fname):    
+
+    df = pd.read_csv(fname)
+    
+    hover = HoverTool(tooltips=[("Index", "$index"),("GDD", "$y"),])
+    p = figure(title = "Montreal GDD 2015", tools=[hover, "pan,reset,resize,wheel_zoom"])
+    p.xaxis.axis_label = 'Date'
+    p.yaxis.axis_label = 'GDD'
+
+    xdata = np.arange(0, len(df["GDD"]))
+
+    p.line(xdata, df["GDD"], legend="GDD", line_color = "red")
+    p.circle(xdata, df["GDD"], legend="GDD", fill_color="red", line_color="red", size=6)
+
+    
+    new_fname =  os.path.dirname(os.path.realpath(__file__)) + "/../Output/" + "bokeh_gdd.html"
+    output_file(new_fname, title="GDD plot")
+
+    save(p)
+
+
+if __name__ == '__main__':
+    main()
