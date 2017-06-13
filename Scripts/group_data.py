1662c37c247b7e7a1b174ae7cbf7589ca8a5c047
Razy-MUN <rbehbahani@mun.ca>
2017-06-13 00:09:54 -0230

    task2. Q6 completion

--- Scripts/group_data.py
+++ Scripts/group_data.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import glob
-from sklearn import datasets, linear_model
-matplotlib inline
+from scipy import stats
+%matplotlib inline
 
 def datalist(file_name):
     data_tmp = pd.read_csv(file_name)
@@ -21,7 +20,13 @@ for file_name in file_list:
     (data_column, index_column, year) = datalist(file_name)
     data[year] = pd.DataFrame(data_column, index = index_column)
 
-data.to_csv("./Output/Ottawa_grouped_gdd.csv")
-plt.plot(7_15)
-plt.show()
-regr = linear_model.LinearRegression()
+x=np.linspace(1990,2015,26)
+y=np.array(z)[-27:-1]
+slope,intercept,r_value,p_value,std_err=stats.linregress(x,y)
+plt.plot(x,y,'bo',label='GDD')
+plt.plot(x,intercept+slope*x,'r',label='linear regression')
+plt.legend()
+plt.title('Ottawa GDD comparison over last 25 years',size=15)
+plt.xlabel('Years',size=18,color='g')
+plt.ylabel('GDD acummulation till June 15th',size=12,color='g')
+plt.savefig('./Output/Ottawa GDD comparison.png')
